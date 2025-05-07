fae8387d4dea55ae5f2eba15e2dab38abbdb87ed
[loris danjou] <[loris_danjou@outlook.com]>
2025-05-07 11:25:05 +0200

    added affine transformation and photo to fiducial transform (IO)

--- geometry/internal_orientation.py
+++ geometry/internal_orientation.py
@@ -1,8 +1,34 @@
 import numpy as np
 
-def image_to_fiducial_coordinates(x, y, xc, yc, alpha, delta_xi, delta_eta):
+# Conversion between photo and fiducial coordinates
+
+def photo_to_fiducial_coordinates(xp, yp, x0, y0):
     '''
-    Convert image coordinates to fiducial coordinates using the given parameters.
+    Convert photo coordinates to fiducial coordinates using the given parameters.
+    Inputs:
+        x, y: Photo coordinates.
+        x0, y0: Location of the optical center in the fiducial coordinate system.
+    Outputs:
+        xi, eta: Fiducial coordinates.
+    '''
+    return xp - x0, yp - y0
+
+def fiducial_to_photos_coordinates(xi, eta, x0, y0):
+    '''
+    Convert fiducial coordinates to photo coordinates using the given parameters.
+    Inputs:
+        xi, eta: Fiducial coordinates.
+        x0, y0: Location of the optical center in the fiducial coordinate system.
+    Outputs:
+        x, y: Photo coordinates.
+    '''
+    return xi + x0, eta + y0
+
+# Conversion between image and fiducial coordinates
+
+def image_to_fiducial_coordinates_molnar(x, y, xc, yc, alpha, delta_xi, delta_eta):
+    '''
+    Convert image coordinates to fiducial coordinates as explained in Molnar et al. (2021), using the given parameters.
     Inputs:
         x, y: Image coordinates.
         xc, yc: Center of the fiducial coordinate system.
@@ -21,9 +47,9 @@ def image_to_fiducial_coordinates(x, y, xc, yc, alpha, delta_xi, delta_eta):
     return XI[0, 0], XI[1, 0]
 
 
-def fiducial_to_image_coordinates(xi, eta, xc, yc, alpha, delta_xi, delta_eta):
+def fiducial_to_image_coordinates_molnar(xi, eta, xc, yc, alpha, delta_xi, delta_eta):
     '''
-    Convert fiducial coordinates to image coordinates using the given parameters.
+    Convert fiducial coordinates to image coordinates as explained in Molnar et al. (2021), using the given parameters.
     Inputs:
         xi, eta: Fiducial coordinates.
         xc, yc: Center of the fiducial coordinate system.
@@ -42,7 +68,7 @@ def fiducial_to_image_coordinates(xi, eta, xc, yc, alpha, delta_xi, delta_eta):
     return X[0, 0], X[1, 0]
 
 
-def objective_function(params, FMs_fiducial_true_coords, FMs_image_true_coords):
+def objective_function_molnar(params, FMs_fiducial_true_coords, FMs_image_true_coords):
     '''
     Objective function for the optimization process to retrieve parameters to switch from a coordinate system and the other.
     Inputs:
@@ -58,7 +84,7 @@ def objective_function(params, FMs_fiducial_true_coords, FMs_image_true_coords):
     xc, yc, alpha, delta_xi, delta_eta = params[0], params[1], params[2], params[3], params[4]
         
     FMs_fiducial_inferred_coords = np.array(
-        [np.array(image_to_fiducial_coordinates(X[0], X[1], xc, yc, alpha, delta_xi, delta_eta)) for X in FMs_image_true_coords]
+        [np.array(image_to_fiducial_coordinates_molnar(X[0], X[1], xc, yc, alpha, delta_xi, delta_eta)) for X in FMs_image_true_coords]
     )
     
     res = 1/2 * np.linalg.norm(FMs_fiducial_inferred_coords - FMs_fiducial_true_coords, axis=1) ** 2
@@ -118,7 +144,7 @@ def objective_function_linear(matrix, FMs_fiducial_true_coords, FMs_image_true_c
     return res
 
 
-def least_squares_linear(FMs_fiducial_true_coords, FMs_image_true_coords, alpha=0, a_priori=np.eye(2)):
+def least_squares_linear(FMs_fiducial_true_coords, FMs_image_true_coords, alpha=0, a_priori=np.eye(2)): # not working for real image
     '''
     Compute the least squares solution to find the linear transformation matrix that maps image coordinates to fiducial coordinates.
     Inputs:
@@ -143,8 +169,64 @@ def least_squares_linear(FMs_fiducial_true_coords, FMs_image_true_coords, alpha=
         x0 = a_priori.reshape(-1, 1)
         x = np.linalg.inv(A.T @ A + alpha * np.eye(4)) @ (A.T @ b + alpha * x0)
         
-    matrix = x.reshape(-1, 2)
-    
-    # print("cond = ", np.linalg.norm(np.linalg.inv(A.T @ A)) * np.linalg.norm(A.T @ A))
-    
+    matrix = x.reshape(-1, 2)    
     return matrix
+
+
+def image_to_fiducial_coordinates_affine(x, y, matrix, translation_vector=np.array([[0], [0]])):
+    '''
+    Convert image coordinates to fiducial coordinates using the given parameters.
+    Inputs:
+        x, y: Image coordinates.
+        matrix: Linear transformation matrix (np.array 2x2).
+        translation_vector: Translation vector (np.array 2x1).
+    Outputs:
+        xi, eta: Fiducial coordinates.
+    '''
+    XI = np.zeros((2, 1))
+    XI = matrix @ np.array([[x], [y]]) - translation_vector
+    return XI[0, 0], XI[1, 0]
+
+
+def fiducial_to_image_coordinates_affine(xi, eta, matrix, translation_vector=np.array([[0], [0]])):
+    '''
+    Convert fiducial coordinates to image coordinates using the given parameters.
+    Inputs:
+        xi, eta: Fiducial coordinates.
+        matrix: Linear transformation matrix used to convert from image to fiducial coordinates (np.array 2x2).
+        translation_vector: Translation vector (np.array 2x1).
+    Outputs:
+        x, y: Image coordinates.
+    '''
+    X = np.zeros((2, 1))
+    X = np.linalg.inv(matrix) @ (np.array([[xi], [eta]]) + translation_vector)
+    return X[0, 0], X[1, 0]
+
+
+def objective_function_affine(params, FMs_fiducial_true_coords, FMs_image_true_coords):
+    '''
+    Objective function for the optimization process to retrieve parameters to switch from a coordinate system and the other.
+    Inputs:
+        m11, m12, m21, m22: Linear transformation matrix coefficients.
+        xt, yt: Translation vector components.
+        FMs_fiducial_true_coords: True fiducial marks fiducial coordinates (np.array of shape p x 2, p being the number of fiducial marks).
+        FMs_image_true_coords: True fiducial marks image coordinates (np.array of shape p x 2).
+    Outputs:
+        res: Sum of squared differences between inferred and true fiducial coordinates.
+    '''
+    assert FMs_fiducial_true_coords.shape == FMs_image_true_coords.shape, "Fiducial and image coordinates must have the same shape."
+    assert FMs_fiducial_true_coords.shape[1] == 2, "Fiducial and image coordinates must have two columns."
+    
+    m11, m12, m21, m22, xt, yt = params[0], params[1], params[2], params[3], params[4], params[5]
+    matrix = np.array([[m11, m12], [m21, m22]])
+    translation_vector = np.array([[xt], [yt]])
+        
+    FMs_fiducial_inferred_coords = np.array(
+        [np.array(image_to_fiducial_coordinates_affine(X[0], X[1], matrix, translation_vector)) for X in FMs_image_true_coords]
+    )
+    
+    res = 1/2 * np.linalg.norm(FMs_fiducial_inferred_coords - FMs_fiducial_true_coords, axis=1) ** 2
+    res = np.sum(res)
+    return res
+
+
