9b4ebaa4a321792b6b5575ddad9a545018f00bb0
[loris danjou] <[loris_danjou@outlook.com]>
2025-06-10 09:47:28 +0200

    check eo geom

--- geometry/external_orientation.py
+++ geometry/external_orientation.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+A = 6378137.0 
+F = 1 / 298.257223563
+E = np.sqrt(2 * F - F**2)
+B = A * np.sqrt(1 - E**2)
+EPS = E**2 / (1 - E**2)
+
 def geodetic_to_geocentric_cartesian_coordinates(lat, lon, h):
     '''
     Convert ellipsoidal coordinates (latitude, longitude, height) to geocentric Cartesian coordinates (x, y, z).
@@ -10,16 +16,11 @@ def geodetic_to_geocentric_cartesian_coordinates(lat, lon, h):
     Outputs:
         x, y, z: Geocentric Cartesian coordinates in meters.
     '''
-    # WGS84 ellipsoid parameters
-    a = 6378137.0 
-    f = 1 / 298.257223563
-    
-    e = np.sqrt(2 * f - f**2)
-    nu = a / np.sqrt(1 - e**2 * np.sin(lat)**2)
+    nu = A / np.sqrt(1 - E**2 * np.sin(lat)**2)
     
     x = (nu + h) * np.cos(lat) * np.cos(lon)
     y = (nu + h) * np.cos(lat) * np.sin(lon)
-    z = ((1 - e**2) * nu + h) * np.sin(lat)
+    z = ((1 - E**2) * nu + h) * np.sin(lat)
     return x, y, z
 
 def geocentric_cartesian_to_geodetic_coordinates(x, y, z):
@@ -32,20 +33,13 @@ def geocentric_cartesian_to_geodetic_coordinates(x, y, z):
         lon: Longitude in radians.
         h: Height above the ellipsoid in meters.
     '''
-    # WGS84 ellipsoid parameters
-    a = 6378137.0 
-    f = 1 / 298.257223563
-    
-    e = np.sqrt(2 * f - f**2)
-    b = a * np.sqrt(1 - e**2)
     p = np.sqrt(x**2 + y**2)
-    beta = np.arctan(z / ((1 - f) * p))
-    eps = e**2 / (1 - e**2)
+    beta = np.arctan(z / ((1 - F) * p))
     
-    lat = np.arctan((z + eps * b * np.sin(beta)**3) / (p - e**2 * a * np.cos(beta)**3))
+    lat = np.arctan((z + EPS * B * np.sin(beta)**3) / (p - E**2 * A * np.cos(beta)**3))
     lon = np.arctan(y / x)
-    nu = a / np.sqrt(1 - e**2 * np.sin(lat)**2)
-    h = p * np.cos(lat) + z * np.sin(lat) - (a**2/nu)
+    nu = A / np.sqrt(1 - E**2 * np.sin(lat)**2)
+    h = p * np.cos(lat) + z * np.sin(lat) - (A**2/nu)
     
     return lat, lon, h
 
